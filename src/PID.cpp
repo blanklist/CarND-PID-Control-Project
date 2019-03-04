4a0b1218937a0c0a3e0ef290aadfee5129055e94
blanklist <joehineriv@gmail.com>
2019-03-03 17:41:39 -0800

    first draft

--- src/PID.cpp
+++ src/PID.cpp
@@ -9,22 +9,26 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp_, double Ki_, double Kd_) {
-  /**
-   * TODO: Initialize PID coefficients (and errors, if needed)
-   */
-
+  // Initialize PID coefficients (and errors, if needed)
+  
+  Kp = Kp_;
+  Ki = Ki_;
+  Kd = Kd_;
 }
 
 void PID::UpdateError(double cte) {
-  /**
-   * TODO: Update PID errors based on cte.
-   */
-
+  // Update PID errors based on cte.
+  
+  // delta of old error from current
+  d_error = (cte - p_error);
+  // update p_error
+  p_error = cte;
+  // sum of cte
+  i_error += cte;
 }
 
 double PID::TotalError() {
-  /**
-   * TODO: Calculate and return the total error
-   */
-  return 0.0;  // TODO: Add your total error calc here!
+  // Calculate and return the total error
+
+  return Kp * p_error + Kd * d_error + Ki * i_error;
 }
